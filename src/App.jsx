ab2e32643f979cf79da8b1620c3757a796dfc52d
Arturo Lopez <jhonas29@gmail.com>
2023-05-25 17:43:47 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.module.scss';
+
+function App() {
+
+	return (
+		<main>
+			<h1>Reliable, efficient delivery<br></br> Powered by Technology</h1>
+		</main>
+	);
+}
+
+export default App;
